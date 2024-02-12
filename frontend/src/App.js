aeae2b252cd5e617aa0395822e0d8b226c8f8f7f
sairam balu <balusairam26@gmail.com>
2024-02-12 17:28:40 -0500

    added frontend code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,3 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import "./App.css"
 
